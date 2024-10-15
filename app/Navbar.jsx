b9f4d73711937a13b67ba40dedee5b5a39973403
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-10-15 15:01:31 -0400

    changed Navbar and styling

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,20 +1,21 @@
+'use client';
+import { usePathname } from "next/navigation";
+
 export default function Navbar() {
+  const pathname = usePathname()
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
-        Mini-Games
-      </a>
       <ul>
-        <li>
+        <li className={`${pathname === '/tictactoe' ? 'active' : ''}`}>
           <a href="/tictactoe">Tic-Tac-Toe</a>
         </li>
-        <li>
+        <li className={`${pathname === '/madlib' ? 'active' : ''}`}>
           <a href="/madlib">Mad Lib</a>
         </li>
-        <li>
+        <li className={`${pathname === '/magicEightBall' ? 'active' : ''}`}>
           <a href="/magicEightBall">Magic Eight Ball</a>
         </li>
-        <li>
+        <li className={`${pathname === '/rockPaperScissors' ? 'active' : ''}`}>
           <a href="/rockPaperScissors">Rock-Paper-Scissors</a>
         </li>
       </ul>
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div id="home">
     <h1>Mini-Games</h1>
     <h2>Choose a game from the menu to get started!</h2>
     </div>
